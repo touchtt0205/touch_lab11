2ef18b76ee78d350d01759f17f7101a541766873
touch <tttouch1122@gmail.com>
2023-01-03 16:41:51 +0700

    11_3

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,27 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include<cmath>
+#include<cstdlib>
+#include<fstream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    double mean;
+    double x2;
+    double sum;
+    int count;
+    string txte;
+    ifstream source("score.txt");
+    while(getline(source,txte)){
+        sum += atof(txte.c_str());
+        x2 += pow(atof(txte.c_str()),2);
+        count++;
+    }
+    mean = sum/count;
+    cout << "Number of data = "<<count<<"\n";
+    cout << setprecision(3);
+    cout << "Mean = "<<mean<<"\n";
+    cout << "Standard deviation = "<<sqrt((x2/count)-pow(mean,2));
+}
